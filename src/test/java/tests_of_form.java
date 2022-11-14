ceabfe2eb7bd8405903895542d18deb55012ee80
Aleksei Dementev <karabass18@gmail.com>
2022-11-15 00:52:40 +0500

    add firstName

--- src/test/java/tests_of_form.java
+++ src/test/java/tests_of_form.java
@@ -1,8 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class tests_of_form {
-
+@Test
     void fillFormTest() {
-        open("http://google.com");
+        open("https://demoqa.com/automation-practice-form");
+        $("#firstName").setValue("Aleksei");
     }
 }
