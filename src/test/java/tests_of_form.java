fa058a11a14c18710ce80e5b6c6529865e071762
Aleksei Dementev <karabass18@gmail.com>
2022-11-16 01:17:51 +0500

    add calendar

--- src/test/java/tests_of_form.java
+++ src/test/java/tests_of_form.java
@@ -1,3 +1,4 @@
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -6,7 +7,22 @@ import static com.codeborne.selenide.Selenide.open;
 public class tests_of_form {
 @Test
     void fillFormTest() {
+        Configuration.holdBrowserOpen= true;
+        Configuration.browserSize = "1920x1080";
         open("https://demoqa.com/automation-practice-form");
-        $("#firstName").setValue("Aleksei");
+        $("#firstName").setValue("Vania");
+        $("#lastName").setValue("Ivanov");
+        $("#userEmail").setValue("vania@vania.com");
+        $("#gender-radio-3").doubleClick();
+
+        $("#userNumber").setValue("1234567890");
+        $("#dateOfBirthInput").click();
+        $("[aria-label=\"Choose Thursday, November 10th, 2022\"]").click();
+        $("#subjectsContainer").click();
+        $("#subjectsContainer").setValue("c");
+
+
+
+        //$("#dateOfBirthInput").setValue("01.01.23");
     }
 }
