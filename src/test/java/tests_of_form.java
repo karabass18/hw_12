2a9686a77d34402987e78d9ab631b9c4c17dac63
Aleksei Dementev <karabass18@gmail.com>
2022-11-17 09:45:59 +0500

    add calendar

--- src/test/java/tests_of_form.java
+++ src/test/java/tests_of_form.java
@@ -1,6 +1,8 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -8,7 +10,7 @@ public class tests_of_form {
 @Test
     void fillFormTest() {
         Configuration.holdBrowserOpen= true;
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = "780x1080";
         open("https://demoqa.com/automation-practice-form");
         $("#firstName").setValue("Vania");
         $("#lastName").setValue("Ivanov");
@@ -19,14 +21,18 @@ public class tests_of_form {
         $("#dateOfBirthInput").click();
         $("[aria-label=\"Choose Thursday, November 10th, 2022\"]").click();
         $("#subjectsContainer").click();
+        $("#uploadPicture").scrollIntoView(true);
         $("#subjectsContainer #subjectsInput" ).setValue("Chemistry").pressEnter();
 
         $("[for=\"hobbies-checkbox-1\"]").click();
-        $("#uploadPicture").scrollIntoView(true);
-        $("#uploadPicture").doubleClick();
-        $("#uploadPicture").setValue("Screenshot_1.jpg");
 
+        //$("#uploadPicture").doubleClick();
+        $("#uploadPicture").uploadFile(new File("src/test/files/squid-game-anime.jpg"));
+        $("#currentAddress").setValue("Home Street");
+        $("#submit").scrollIntoView(true);
 
+        //$("#state ").setValue("n").pressEnter();
+        $("#submit").click();
         //$("#dateOfBirthInput").setValue("01.01.23");
     }
 }
