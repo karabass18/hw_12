e0f7bc12bbf874e2fc10452a152bf85724db2db8
Aleksei Dementev <karabass18@gmail.com>
2022-11-18 01:23:45 +0500

    add vars

--- src/test/java/tests_of_form.java
+++ src/test/java/tests_of_form.java
@@ -3,6 +3,9 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
+import static com.codeborne.selenide.Condition.selected;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -11,15 +14,40 @@ public class tests_of_form {
     void fillFormTest() {
         Configuration.holdBrowserOpen= true;
         Configuration.browserSize = "780x1080";
+
+        String userName = "Vania";
+        String userLastName = "Ivanov";
+        String userEmail = "vania@vania.com";
+        String userGen = "Other";
+        String userPhone = "0123456789";
+        String userBDDay = "01";
+        String userBDMonth = "May";
+        String userBDYear = "1913";
+        String userSubj = "Chemistry";
+        String userHobb = "Sports";
+        String picPath = "src/test/files/";
+        String picName = "squid-game-anime.jpg";
+        String userAdd = "Home Street";
+        String userState = "Haryana";
+        String userCity = "Karnal";
+
+
         open("https://demoqa.com/automation-practice-form");
-        $("#firstName").setValue("Vania");
-        $("#lastName").setValue("Ivanov");
+        $("#firstName").setValue(userName);
+        $("#lastName").setValue(userLastName);
         $("#userEmail").setValue("vania@vania.com");
         $("#gender-radio-3").doubleClick();
 
         $("#userNumber").setValue("1234567890");
         $("#dateOfBirthInput").click();
-        $("[aria-label=\"Choose Thursday, November 10th, 2022\"]").click();
+        //$("[aria-label=\"Choose Thursday, December 10th, 2010\"]").click();
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").selectOption("May");
+        $(".react-datepicker__year-select").click();
+        //$(".react-datepicker__year-select byText(\"1913\")").scrollTo();
+        $(".react-datepicker__year-select").selectOption("1913");
+        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__day--0" + "01").click();
         $("#subjectsContainer").click();
         $("#uploadPicture").scrollIntoView(true);
         $("#subjectsContainer #subjectsInput" ).setValue("Chemistry").pressEnter();
@@ -31,8 +59,13 @@ public class tests_of_form {
         $("#currentAddress").setValue("Home Street");
         $("#submit").scrollIntoView(true);
 
-        //$("#state ").setValue("n").pressEnter();
+        $("#state ").click();
+        $(byText("Haryana")).click();
+        $("#city ").click();
+        $(byText("Karnal")).click();
         $("#submit").click();
+        //$(".table-dark.")
+        $("tr:nth-child(2) td:nth-child(2)").shouldHave(text("vania@vania.com"));
         //$("#dateOfBirthInput").setValue("01.01.23");
     }
 }
