74afa3052cc15e7b382eab399e8f01ab752cb6a1
Aleksei Dementev <karabass18@gmail.com>
2022-12-17 16:09:44 +0500

    first

--- src/test/java/StartsSet.java
+++ src/test/java/StartsSet.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 public class StartsSet {
     @BeforeAll
     static void beforeAll() {
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         //Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "720x800";
         SelenideLogger.addListener("allure", new AllureSelenide());
